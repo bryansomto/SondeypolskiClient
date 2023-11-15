cd63adb905ca78a0c73233cb823812bfefe70502
bryansomto <bryansomto@gmail.com>
2023-11-15 12:53:20 +0100

    modified app information

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import { StyleSheetManager, createGlobalStyle } from "styled-components";
 import { CartContextProvider } from "../lib/CartContext";
 import isPropValid from "@emotion/is-prop-valid";
-// import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const GlobalStyles = createGlobalStyle`
     body{
@@ -13,22 +12,8 @@ const GlobalStyles = createGlobalStyle`
 `;
 
 export default function App({ Component, pageProps }) {
-  // const helmetContext = {};
   return (
     <>
-      {/* <HelmetProvider context={helmetContext}>
-        <Helmet>
-          <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossorigin
-          ></link>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-            rel="stylesheet"
-          ></link>
-        </Helmet> */}
       <StyleSheetManager
         shouldForwardProp={isPropValid}
         disableVendorPrefixes={false}
@@ -38,7 +23,6 @@ export default function App({ Component, pageProps }) {
           <Component {...pageProps} />
         </CartContextProvider>
       </StyleSheetManager>
-      {/* </HelmetProvider> */}
     </>
   );
 }
